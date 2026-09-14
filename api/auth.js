0f81307658f799ffc9fdaeaa7e77199d5845c692
Mourchid Raimi <mourchidraimi0@gmail.com>
2026-09-14 12:53:25 +0200

    Pointe toutes les références du domaine vers www.novabk.pro
    novabk.pro redirige (308) vers www.novabk.pro : les liens utilisent
    désormais directement la forme canonique, évitant un aller-retour réseau.

--- api/auth.js
+++ api/auth.js
@@ -7,7 +7,7 @@ const {
   ensureSchema, logActivity, readJsonBody, send, fail
 } = require('./_lib/db');
 
-const SITE_URL = 'https://novabk.pro';
+const SITE_URL = 'https://www.novabk.pro';
 
 function verificationEmailHtml(firstName, link) {
   return `
